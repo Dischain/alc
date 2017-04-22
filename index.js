f4dc41582dc207851b84988cd141935030d6d8c0
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-04-22 11:28:32 +0300

    Implemented full script logic

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 #!/usr/bin/env node
 
-const cli = require('./cli');
+const cli = require('./lib/cli');
+const isFileSync = require('./lib/util').isFileSync;
+
+const countLinesRecursively = require('./lib/counter').countLinesRecursively;
+const countLines = require('./lib/counter').countLines;
+const countAtSingleFile = require('./lib/counter').countAtSingleFile;
 
 cli.processInput(process.argv, console.log);
-console.log(cli.getConsoleArgs(process.argv));
 
-cli.printResult([{
-		ext: 'js',
-		files: 12,
-		lines: 10
-	},
-	{
-		ext: 'json',
-		files: 11,
-		lines: 15
-	}],
-	console.log);
+const args = cli.getConsoleArgs(process.argv, console.log);
+const targetPath = args.targetPath;
+if (targetPath && !args.error) {
+    if (isFileSync(targetPath)) {
+        countAtSingleFile(targetPath).then((result) => {
+            cli.printResult(result, console.log);
+            process.exit(0);
+        });
+    } else {
+        countLinesRecursively(targetPath, args).then((result) => {
+            cli.printResult(result, console.log);
+        });
+    }
+
+} else {
+    console.log('exit with error');
+    process.exit(1);
+}
