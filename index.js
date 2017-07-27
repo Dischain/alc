29d0db9fd468fb48d6fd1a635d75f5c34f46be10
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-07-27 22:17:51 +0300

    converted indents to spaces

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ cli.processInput(process.argv, console.log);
 const args = cli.getConsoleArgs(process.argv, console.log);
 const targetPath = args.targetPath;
 if (targetPath && !args.error) {
-    if (isFileSync(targetPath)) {
-        countAtSingleFile(targetPath).then((result) => {
-            cli.printResult(result, console.log);
-            process.exit(0);
-        });
-    } else {
-        countLinesRecursively(targetPath, args).then((result) => {
-            cli.printResult(result, console.log);
-            process.exit(0);
-        });
-    }
+  if (isFileSync(targetPath)) {
+    countAtSingleFile(targetPath).then((result) => {
+      cli.printResult(result, console.log);
+      process.exit(0);
+    });
+  } else {
+    countLinesRecursively(targetPath, args).then((result) => {
+      cli.printResult(result, console.log);
+      process.exit(0);
+    });
+  }
 
 } else {
-    console.log('exit with error');
-    process.exit(1);
+  console.log('exit with error');
+  process.exit(1);
 }
